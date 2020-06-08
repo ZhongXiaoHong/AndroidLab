16fa89660aaf410c72d832c565b086c460e656e7
zhongxiaohong <931766850@qq.com>
2020-06-08 17:50:31 +0800

    s

--- java_lib/src/main/java/com/example/java_lib/concurrent_coding/cyclicbarrier/YourRun.java
+++ java_lib/src/main/java/com/example/java_lib/concurrent_coding/cyclicbarrier/YourRun.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CyclicBarrier;
 public class YourRun {
 
     public static void main(String[] args) {
-        
+
         //也就是5个线程都执行了cbRef对象的await()方法后程序才可以继续向下运行，
         // 否则这些线程彼此互相等待，一直呈阻塞状态
         CyclicBarrier cbRef = new CyclicBarrier(5, new Runnable() {
